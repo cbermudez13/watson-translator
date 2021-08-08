37b174515feddec2e05315db7b9402abaae540e9
Carlos Bermudez <cbermudezrivas@gmail.com>
2021-08-08 10:51:17 -0400

    Fileupload to uploads folder, changes to package.json on client

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting started
+# Installing express
+    npm i express express-fileupload
+
+# Installing nodemon and concurrently to allow us to run server and client at same time in script
+    npm i -D nodemon concurrently 
+    
--- client/src/components/fileupload.js
+++ client/src/components/fileupload.js
@@ -1,18 +1,38 @@
 import React, { Fragment, useState } from 'react';
+ 
+
+const axios = require('axios');
 
 export default function  Fileupload(){
-   const [file, setFile] = useState({});
-   const [filename, setFilename] = useState("Choose file");
+   const [file, setFile] = useState('');
+   const [filename, setFilename] = useState('Choose File');
+   const [uploadedFile, setUploadedFile] = useState({});
 
-    const onChange = (f) => {
+    const onChange = f => {
         setFile(f.target.files[0]);
         setFilename(f.target.files[0].name);
     }
 
-    const onSubmit = (f) => {
-        f.preventDefaut();
+    const onSubmit = async f => {
+        f.preventDefault();
         const formData = new FormData();
-        formData.append(file);
+        formData.append('file', file);
+
+        try{
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            const { fileName, filePath } = res.data;
+            setUploadedFile({ fileName, filePath });
+        } catch(e){
+            if(e.response.status === 500){
+                console.log('Server Error');
+            } else {
+                console.log(e.response.data.msg);
+            }
+        }
     }
 
 
